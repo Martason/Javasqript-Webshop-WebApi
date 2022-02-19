9a051c448ebc04f6e8070a3b3575231952e2de41
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2022-02-19 11:37:01 +0100

    Skapat logic och script klass - städat lite - kan presentera 10 pokemons - Problem med read more kappen

--- logic.js
+++ logic.js
@@ -1,79 +1,39 @@
-//Metod för att hämta pokemons
 
+export class logic {
 
-//
-
-
-//Hämta en array av spec lenght av pokemon JsonObj från pokeApi:n
-//argument in vilken "sida" usern står på. typ 1-10, 11-20, osv osv rimligt? 
-// return fetch(pokemonJson.species.url); //TODO detta är för att utveckla en readmore knapp senare
-
-// 
-// The Promise.all() method takes an iterable of promises as an input, 
-//and returns a single Promise that resolves to an array of the results of the input promises. 
-// 
-
-/* const fetchPokemon = (id) =>{
-
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-      fetch(url)
-      .then((response) => response.json())
-      .then(pokemonJsonObj => {
-          console.log(pokemonJsonObj);
-          return pokemonJsonObj;
-
-      });
-} */
-
-/* 1 = 1-10
-2 = 11-20
-3 = 21-30
-4 = 31-40
-5 = 41-50
+/**
+ * 
+ * @param {*} id 
+ * @returns promise of that pokemon
  */
-
-/* const getPokemonArray = (pageNr) =>{
-
-    pageNr * 10;
-
-const pokemons = [];
-    for (let i = pageNr - 9; i <= pageNr; i++){
-
-        pokemons.push = (fetchPokemon(i));
-    }
-return pokemons;
-
-} */
-
-const fetchPokemon = async id =>{
+fetchPokemon = async (id) =>{
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const result = await fetch(url);
     const pokemon = await result.json();
     return pokemon;
 }
+/**
+ * 
+ * @param {*} pageNr 
+ * @returns promiseArray of the ten pokemons on that page
+ */
 
-const getPokemons = async (pageNr) => {
-
-
+getPokemons = async (pageNr) => {
 
     const pokemons = [];
     pageNr = pageNr * 10;
     for (let i = pageNr -9; i <= pageNr; i++)
     {
-        const pokemon = await fetchPokemon(i)
+        const pokemon = await this.fetchPokemon(i)
         pokemons.push(pokemon);
     }
 
-    return (pokemons); //Ett jävla promise!! behöver en .then funktion till på nått sätt. 
-
+    return (pokemons); //TODO men hur ska man tänka här med att man returnerar ett promise?
 };
 
-getPokemons(10).then(function(result){
-    for(let pokemon of result)
-    {
-        console.log(pokemon)
-    }
-})
+
+}
+
 
 
 
--- script.js
+++ script.js
@@ -6,3 +6,62 @@
 
 // DOM för readmore knappen
 // DOME att presentera info om pokemon på sidan
+
+import { logic as Shop } from "./logic.js";
+
+let shop;
+shop = new Shop();
+
+
+document.querySelectorAll(".read-more").forEach(btn => btn.addEventListener('click', readMore)); // TODO detta funkar inte. förmodligen för att readmore kbappe ninte finns innan detta? Den skapas ju efteåt... 
+//eventlistnener för att ta in pagenu från en pagnation typ och koppla samman med updatePage
+
+function handleReadMore(e){
+
+    const clickedPokemonId = e.target.id;
+    console.log(clickedPokemonId);
+}
+
+function moreInfoPopUp(pokemon)
+{
+
+}
+
+function updatePage(e) //TODO kommer ta in event från pagnation. 
+{
+const pageNr = e // e.target.id;
+
+let pokemonDiv = "";
+
+shop.getPokemons(2).then(function(pokemons){
+    for(let pokemon of pokemons)
+    {
+        console.log(pokemon)
+
+        pokemonDiv = `
+        <div class="product-container">
+        <h3 class="name">${pokemon.name}</h3>
+        <img class="sprite" src="${pokemon.sprites.front_default}">
+        <div class="type">${pokemon.type}</div>
+        <div class="price">999 SEK</div>
+        <button id="${pokemon.id}" class="button read-more">Readmore</button>
+        <button class="button add-to-shart">AddToShart</button> 
+    `;
+
+    document.getElementById("pokemonShop").insertAdjacentHTML(  "beforeend", pokemonDiv)
+
+    }
+}); 
+
+
+    
+}
+
+updatePage(8)
+
+
+/* 
+   
+*/
+
+
